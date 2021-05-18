c43a2eb4316d9415e472f6a4cc41ef1bf1ed5977
sabrrinaahmed <sabrrinaahmed@tamu.edu>
2021-05-18 18:47:34 -0500

    starting zigzag conversion

---
+++ medium/zigzagconv.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+string solution(string s, int numRows){
+    
+}
+
+int main(){
+    string s;
+    int numRows;
+    string result = solution(s, numRows);
+    cout << result << endl;
+}
